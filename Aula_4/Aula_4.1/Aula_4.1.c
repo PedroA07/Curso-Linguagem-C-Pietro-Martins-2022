cc91b875a5454bac50f6b74bb9c003524f249f10
Pedro Andrade <pedro.andrade.pereira@hotmail.com.br>
2024-09-17 10:14:03 -0300

    Aula 4.1

--- Aula_4/Aula_4.1/Aula_4.1.c
+++ Aula_4/Aula_4.1/Aula_4.1.c
@@ -1,2 +1,36 @@
 //Domine o comando IF em C
 
+/*
+        ESTRUTURA DE DECISÃO SIMPLES
+    Comando "if": se a condição for verdadeira, o bloco é executado, senão, é ignorado.
+
+    Sintaxe:
+        if (<condição>){
+            <bloco_de_comandos>
+        }
+*/
+/*
+        COMO CRIAR CONDIÇÕES LÓGICO-RELACIONAIS
+    Operadores Relacionais:
+        - Maior(>): ">";
+        - Maior ou igual(>=): ">=";
+        - Menor(<): "<";
+        - menor ou igual(<=): "<=";
+        - Igual(=): "==";
+        - Diferente(!): "!=";
+*/
+
+//EXEMPLO:
+#include <stdio.h>
+
+int main(){
+
+    float m;
+
+    printf("Insira a nota: \n")
+    scanf("%f", &m);
+
+    if(m >= 7.0){
+        printf("Aprovado!\n");
+    }
+}
