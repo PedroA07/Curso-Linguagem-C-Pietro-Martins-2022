df6cacb0f1a6a77b956215311cc1d2b0bd8db7de
Pedro Andrade <pedro.andrade.pereira@hotmail.com.br>
2024-09-22 16:12:22 -0300

    Aula_9.2

---
+++ Aula_9/Aula_9.2/Aula_9.2.c
@@ -0,0 +1,27 @@
+// Trabalhando com vários arquivos.
+
+/*
+        INDO ALÉM COM A FUNÇÃO: "main()"
+
+    - main(): seu programa programa é apenas uma função!
+        - Parâmetros de um programa:
+            - int argc;
+            - char *argv[];
+        - Sintaxe correta do main():
+            int main(int argc, char *argv[]){...}
+*/
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+
+    int i;
+
+    if(argc > 1){
+        printf("Foram inseridos %d argumentos: ");
+        for(i=0;i<argc;i++){
+            printf("%s\n", argv[i]);
+        }
+    }else{
+        printf("Não foram inserídos argumentos no programa.\n");
+    }
+}
